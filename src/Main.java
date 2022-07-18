c81ac579a325968b4c8f3d1f7fb501230e55ba2d
Павел Козодаев <paveldm744@gmail.com>
2022-07-18 13:07:51 +0300

    bonusmili

---
+++ src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        int amount = 10000;
+        int mili = amount / 20;
+        System.out.println(mili + " Бонусных миль");
+    }
+}
